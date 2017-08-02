41b30c0f74f13072067ad5377ebbf0cd5599bf53
glier <borozdenets@gmail.com>
2017-08-02 14:04:18 +0700

    test

--- src/com/portscanner/main.java
+++ src/com/portscanner/main.java
@@ -10,72 +10,63 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        String ip = args[0];
-        int portFrom = 1;
-        int portTo = 65535;
+
+        int portFrom = 80;
+        int portTo = 80;
         LinkedBlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>();
         List<Integer> ports = new ArrayList<>();
 
-//        try {
-//
-//            if(!InetAddress.getByName(ip).isReachable(null,255, 1000)) {
-//                System.out.println("Хост недоступен.");
-//                System.exit(1);
-//            }
-//
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            System.exit(1);
-//        }
+        try {
+
+            if(!InetAddress.getByName(args[0]).isReachable(null,255, 2000)) {
+                System.out.println("Хост недоступен.");
+                System.exit(1);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
         Long timeStart = Calendar.getInstance().getTimeInMillis();
 
         for (int i = portFrom; i <= portTo ; i++) {
             int port = i;
-                blockingQueue.add(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            InetSocketAddress isa = new InetSocketAddress(ip, port);
-                            Socket socket = new Socket();
-                            socket.connect(isa, 100);
+            blockingQueue.add(() -> {
+                try {
+                    InetSocketAddress isa = new InetSocketAddress(InetAddress.getByName(args[0]), port);
+                    Socket socket = new Socket();
+                    socket.connect(isa, 200);
+                    System.out.println(String.format("Порт %d открыт", port));
+                    ports.add(port);
+                    socket.close();
 
-                            if (socket.isConnected()) {
-                                System.out.println(String.format("Порт %d открыт", port));
-                                ports.add(port);
-                                socket.close();
-                            }
-
-                        } catch (ConnectException e) {
-                            //e.printStackTrace();
-                        } catch (SocketTimeoutException e) {
-                            System.out.println(String.format("Порт %d открыт", port));
-                            ports.add(port);
-                            // e.printStackTrace();
-                        } catch (UnknownHostException e) {
-                            e.printStackTrace();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
+                }  catch (IOException e) {
+                    System.out.println(String.format("Порт %d закрыт", port));
+                }
+            });
 
         }
 
         ThreadPoolExecutor threadPoolExecutor =
-                new ThreadPoolExecutor(4, 8, 500,
+                new ThreadPoolExecutor(200, 256, 500,
                         TimeUnit.MILLISECONDS, blockingQueue );
 
         threadPoolExecutor.prestartAllCoreThreads();
+        threadPoolExecutor.awaitTermination(1, TimeUnit.SECONDS);
         threadPoolExecutor.shutdown();
 
         while (true) {
             if (threadPoolExecutor.isTerminated()) {
                 Long timeEnd  = Calendar.getInstance().getTimeInMillis();
                 System.out.println(String.format("Затрачено %d мс.", (timeEnd - timeStart)));
+
+                for (int i = 0; i < ports.size() ; i++) {
+                    System.out.println(ports.get(i));
+                }
                 break;
             }
         }
